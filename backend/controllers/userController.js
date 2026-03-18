ebc55d3da147c173d4d903be6d645fcf7e60ef7b
Katlicia <yusuful80@gmail.com>
2026-03-18 16:38:09 +0300

    disable details for now

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,36 +1,46 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 exports.getUsers = async (req, res) => {
-    const users = await User.find().select('-password');
-    res.json(users);
-}
-
-exports.getUserById = async(req, res) => {
-    const user = await User.findById(req.params.id).select('-password');
-    res.json(user);
-}
-
-exports.updateUser = async(req, res) => {
-    const user = await User.findById(req.params.id);
-
-    if (user) {
-        user.name = req.body.name || user.name;
-        user.surname = req.body.surname || user.surname;
-        user.email = req.body.email || user.email;
-
-        const updatedUser = await user.save();
-        res.json(updatedUser);
-    } else {
-        res.status(404).json({message: 'User not found.'});
-    }
+  const users = await User.find().select("-password");
+  res.status(200).json(users);
 };
 
-exports.deleteUser = async(req, res) => {
-    const user = await User.findById(req.params.id);
-    if (user) {
-        await user.deleteOne();
-        res.json({message: 'User deleted.'});
-    } else {
-        res.status(404).json({message: 'User not found.'});
-    }
+exports.getUserById = async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (!user) {
+    res.status(404).json({ message: "User not found." });
+  }
+  res.status(200).json(user);
+};
+
+// exports.getUserDetail = async (req, res) => {
+//   const user = await User.findById(req.params.id);
+//   res.status.json(200).json({
+//     user,
+//   });
+// };
+
+exports.updateUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.surname = req.body.surname || user.surname;
+    user.email = req.body.email || user.email;
+
+    const updatedUser = await user.save();
+    res.status(200).json(updatedUser);
+  } else {
+    res.status(404).json({ message: "User not found." });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await user.deleteOne();
+    res.status(200).json({ message: "User deleted." });
+  } else {
+    res.status(404).json({ message: "User not found." });
+  }
 };
