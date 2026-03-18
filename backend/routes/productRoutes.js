a226d521a0d91a8f417fedbb9a532230209c0a10
Katlicia <yusuful80@gmail.com>
2026-03-18 16:29:34 +0300

    product routes added

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+  createReview,
+  adminProducts,
+} = require("../controllers/productController");
+const router = express.Router();
+const { authenticationMiddle, isAdmin } = require("../middleware/auth");
+
+router.get("/products", getProducts);
+router.get("/admin/products", authenticationMiddle, isAdmin, adminProducts);
+router.get("/products/:id", getProductById);
+router.post("/products/new", authenticationMiddle, isAdmin, createProduct);
+router.put("/products/:id", authenticationMiddle, isAdmin, updateProduct);
+router.delete("/products/:id", authenticationMiddle, isAdmin, deleteProduct);
+router.post("/products/newReview", authenticationMiddle, createReview);
+
+module.exports = router;
