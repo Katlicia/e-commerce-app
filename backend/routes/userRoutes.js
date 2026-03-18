fcefbac8fb2f3241bf22d30abcd3c2a101335527
Katlicia <yusuful80@gmail.com>
2026-03-18 16:30:18 +0300

    me route added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,14 +3,17 @@ const {
   getUsers,
   getUserById,
   updateUser,
-  deleteUser
+  deleteUser,
+  getUserDetail,
 } = require("../controllers/userController");
+const { authenticationMiddle } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
+router.get("/me", authenticationMiddle, getUserDetail);
 
 module.exports = router;
