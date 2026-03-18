3b7f0d7d07f1568424efd0e9d83f0d875f69ba22
Katlicia <yusuful80@gmail.com>
2026-03-18 16:29:06 +0300

    forget/reset password added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const {register, login} = require("../controllers/authController");
+const express = require("express");
+const {
+  register,
+  login,
+  logout,
+  forgetPassword,
+  resetPassword,
+} = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.post("/forgetPassword", forgetPassword);
+router.post("/reset/:token", resetPassword);
 
 module.exports = router;
