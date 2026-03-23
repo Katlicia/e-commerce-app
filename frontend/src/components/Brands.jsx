bc91059f44d13420c9faf3e767e01e5033526a87
Katlicia <yusuful80@gmail.com>
2026-03-23 17:33:09 +0300

    brand header added

---
+++ frontend/src/components/Brands.jsx
@@ -0,0 +1,14 @@
+import BrandLogos from "./BrandLogos";
+
+function Brands() {
+  return (
+    <div>
+      <div className="chances-bar container d-flex justify-content-between align-items-center px-2">
+        <h2 className="chances-title">Markalar</h2>
+      </div>
+      <BrandLogos />
+    </div>
+  );
+}
+
+export default Brands;
