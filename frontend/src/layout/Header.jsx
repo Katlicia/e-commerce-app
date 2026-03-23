d128832a8e7a973852e86d094159ccca7a850fcf
Katlicia <yusuful80@gmail.com>
2026-03-23 16:32:12 +0300

    sticky header

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -2,32 +2,10 @@ import { CiHeart, CiSearch } from "react-icons/ci";
 import "../styles/Header.css";
 import profileIcon from "../assets/Header/profile-icon.svg";
 import shoppingCartIcon from "../assets/Header/shopping-cart-icon.svg";
-import shoppingIcon from "../assets/Links/shopping_1.svg";
-import messengerIcon from "../assets/Links/messenger_1.svg";
-import presentIcon from "../assets/Links/present_1.svg";
-
-const categories = [
-  "Gıda Mutfak",
-  "Kağıt Ürünleri",
-  "Ofis Kırtasiye",
-  "Temizlik Ürünleri",
-  "Çok Al Az Öde",
-  "Listeli Ürünler",
-];
-
-const featuredLinks = [
-  { label: "Kampanyalar", colorClass: "header-link-blue", icon: presentIcon },
-  {
-    label: "Ayın Ürünleri",
-    colorClass: "header-link-orange",
-    icon: messengerIcon,
-  },
-  { label: "Hemen Al", colorClass: "header-link-green", icon: shoppingIcon },
-];
 
 function Header() {
   return (
-    <header className="site-header">
+    <header className="site-header fixed-top">
       <div className="container">
         <div className="header-main d-flex align-items-center gap-3 gap-lg-4 py-3 flex-wrap flex-lg-nowrap">
           <form className="header-search flex-grow-1 w-100 w-lg-auto">
@@ -83,36 +61,6 @@ function Header() {
                 <strong>280.00tl</strong>
               </span>
             </a>
-          </div>
-        </div>
-      </div>
-
-      <div className="header-nav-wrapper">
-        <div className="container">
-          <div className="header-nav">
-            <nav className="header-categories">
-              {categories.map((item) => (
-                <a key={item} className="header-nav-link" href="#0">
-                  {item}
-                </a>
-              ))}
-            </nav>
-
-            <div
-              className="header-featured-links"
-              style={{ borderLeft: "1px solid #eef1f3", paddingLeft: "15px" }}
-            >
-              {featuredLinks.map((item) => (
-                <a
-                  key={item.label}
-                  className={`header-nav-link ${item.colorClass}`}
-                  href="#0"
-                >
-                  <img style={{ paddingBottom: "3px" }} src={item.icon} />
-                  {item.label}
-                </a>
-              ))}
-            </div>
           </div>
         </div>
       </div>
