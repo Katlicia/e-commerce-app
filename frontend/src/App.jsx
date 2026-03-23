e188ae5b91be775e368301f1138ee1dec41c4e3a
Katlicia <yusuful80@gmail.com>
2026-03-23 16:32:44 +0300

    render headerlinks component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./components/Home.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Links, Route, Routes } from "react-router-dom";
 import Header from "./layout/Header.jsx";
 import Footer from "./layout/Footer.jsx";
 import Topbar from "./components/Topbar.jsx";
 import Carousel from "./components/Carousel.jsx";
 import Featured from "./components/Featured.jsx";
 import Chances from "./components/Chances.jsx";
-import ProductCard from "./components/ProductCard.jsx";
 import ProductList from "./components/ProductList.jsx";
+import HeaderLinks from "./components/HeaderLinks.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Topbar />
       <Header />
+      <HeaderLinks />
       <Carousel />
       <Featured />
       <Chances />
