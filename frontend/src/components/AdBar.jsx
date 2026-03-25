959dd93e5d04ff7d2bcfbb0efd1415088a5dbc48
Katlicia <yusuful80@gmail.com>
2026-03-25 14:55:39 +0300

    adbanner created

---
+++ frontend/src/components/AdBar.jsx
@@ -0,0 +1,11 @@
+import reklamImage from "../assets/Banners/reklam.png";
+
+function AdBar() {
+  return (
+    <div className="container">
+      <img className="img-fluid" src={reklamImage} alt="reklam" />
+    </div>
+  );
+}
+
+export default AdBar;
