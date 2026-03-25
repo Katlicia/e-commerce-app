9356852a08daa1d5d1f0239b7b68b0c47f99e3ce
Katlicia <yusuful80@gmail.com>
2026-03-25 16:03:26 +0300

    feat: add Home component with layout and product listings

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,42 @@
+import HeaderLinks from "./HeaderLinks";
+import Carousel from "./Carousel";
+import Featured from "./Featured";
+import ProductList from "./ProductList";
+import AdBanners from "./AdBanners";
+import CategoryRow from "./CategoryRow";
+import AdBar from "./AdBar";
+import DealOfDay from "./DealOfDay";
+import Brands from "./Brands";
+import PopularLinks from "./PopularLinks";
+import banner from "../assets/Banners/banner.png";
+import banner1 from "../assets/Banners/banner1.png";
+import banner2 from "../assets/Banners/banner2.png";
+import banner3 from "../assets/Banners/banner3.png";
+import liptonBanner from "../assets/Banners/lipton-banner.png";
+import cifBanner from "../assets/Banners/cif-banner.png";
+
+function Home() {
+  return (
+    <>
+      <HeaderLinks />
+      <Carousel />
+      <Featured />
+      <ProductList
+        title="Kaçırılmayacak Fırsatlar"
+        settings={{ showTimer: true, banner: banner }}
+      />
+      <ProductList title="Yeni Gelenler" />
+      <AdBanners banners={[banner1, banner2, banner3]} />
+      <ProductList title="Önerilen Ürünler" />
+      <CategoryRow left="Okul Kırtasiye" right="Temizlik" />
+      <AdBar />
+      <ProductList title="Son Gezdiğin Ürünler" />
+      <AdBanners banners={[liptonBanner, cifBanner]} />
+      <DealOfDay />
+      <Brands />
+      <PopularLinks />
+    </>
+  );
+}
+
+export default Home;
