dcdd7afe4a5738b448da14a6a3c204b9ebb0dc7a
Katlicia <yusuful80@gmail.com>
2026-03-25 16:01:16 +0300

    feat: add authReducer to the Redux store for user authentication management

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productSlice } from "./productSlice";
+import authReducer from "./authSlice";
 
 export const store = configureStore({
   reducer: {
     product: productSlice.reducer,
+    auth: authReducer,
   },
 });
