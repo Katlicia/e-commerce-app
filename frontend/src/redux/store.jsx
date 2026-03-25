bc626d6fa1a913b555a03fbb9b962fb0f16e328c
Katlicia <yusuful80@gmail.com>
2026-03-25 17:15:52 +0300

    added cart reducer to store

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createReducer } from "@reduxjs/toolkit";
 import { productSlice } from "./productSlice";
 import authReducer from "./authSlice";
+import cartReducer from "./cartSlice";
 
 export const store = configureStore({
   reducer: {
     product: productSlice.reducer,
     auth: authReducer,
+    cart: cartReducer,
   },
 });
