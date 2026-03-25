76e2cf79b58a29539eff3890c6ba0de41feddb91
Katlicia <yusuful80@gmail.com>
2026-03-25 16:02:43 +0300

    feat: implement routing for login and registration in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
 import Header from "./layout/Header.jsx";
 import Footer from "./layout/Footer.jsx";
 import Carousel from "./components/Carousel.jsx";
@@ -14,35 +17,21 @@ import PopularLinks from "./components/PopularLinks.jsx";
 import TopBar from "./components/Topbar.jsx";
 import AdBar from "./components/AdBar.jsx";
 import DealOfDay from "./components/DealOfDay.jsx";
-import banner from "./assets/Banners/banner.png";
-import banner1 from "./assets/Banners/banner1.png";
-import banner2 from "./assets/Banners/banner2.png";
-import banner3 from "./assets/Banners/banner3.png";
-import liptonBanner from "./assets/Banners/lipton-banner.png";
-import cifBanner from "./assets/Banners/cif-banner.png";
+
+import Home from "./components/Home.jsx";
 
 function App() {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <BrowserRouter>
       <TopBar />
       <Header />
-      <HeaderLinks />
-      <Carousel />
-      <Featured />
-      <ProductList
-        title="Kaçırılmayacak Fırsatlar"
-        settings={{ showTimer: true, banner: banner }}
-      />
-      <ProductList title="Yeni Gelenler" />
-      <AdBanners banners={[banner1, banner2, banner3]} />
-      <ProductList title="Önerilen Ürünler" />
-      <CategoryRow left="Okul Kırtasiye" right="Temizlik" />
-      <AdBar />
-      <ProductList title="Son Gezdiğin Ürünler" />
-      <AdBanners banners={[liptonBanner, cifBanner]} />
-      <DealOfDay />
-      <Brands />
-      <PopularLinks />
+      <Routes>
+        <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <RegisterPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
