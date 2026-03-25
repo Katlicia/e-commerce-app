b889bb323de45db73342eb0d790d3512f37318c4
Katlicia <yusuful80@gmail.com>
2026-03-25 12:19:07 +0300

    productlist props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./layout/Header.jsx";
 import Footer from "./layout/Footer.jsx";
 import Topbar from "./components/Topbar.jsx";
 import Carousel from "./components/Carousel.jsx";
 import Featured from "./components/Featured.jsx";
-import Chances from "./components/Chances.jsx";
 import ProductList from "./components/ProductList.jsx";
 import HeaderLinks from "./components/HeaderLinks.jsx";
-import NewDeals from "./components/NewDeals.jsx";
 import AdBanners from "./components/AdBanners.jsx";
-import SuggestedProducts from "./components/SuggestedProducts.jsx";
 import Brands from "./components/Brands.jsx";
 import PopularLinks from "./components/PopularLinks.jsx";
 
@@ -23,18 +20,12 @@ function App() {
       <HeaderLinks />
       <Carousel />
       <Featured />
-      <Chances />
-      <ProductList />
-      <NewDeals />
-      <ProductList />
+      <ProductList title="Kaçırılmayacak Fırsatlar" showTimer />
+      <ProductList title="Yeni Gelenler" />
       <AdBanners />
-      <SuggestedProducts />
-      <ProductList />
+      <ProductList title="Önerilen Ürünler" />
       <Brands />
       <PopularLinks />
-      {/* <Routes>
-        <Route path="/" element={} />
-      </Routes> */}
       <Footer />
     </BrowserRouter>
   );
