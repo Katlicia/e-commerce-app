1d4f8d1d1cfb3563fa08a0332e20a3d00775fd2d
Katlicia <yusuful80@gmail.com>
2026-03-25 11:50:02 +0300

    footer sections added as components

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -1,9 +1,28 @@
+import ReliabilitySection from "../components/ReliabilitySection";
+import footerIcon from "../assets/Footer/footer.png";
+import "../styles/Footer.css";
+import InfoFooter from "../components/InfoFooter";
+
 function Footer() {
   return (
     <div>
-      Footer
+      <ReliabilitySection />
+      <InfoFooter />
+      <div className="border-top py-3">
+        <div className="container d-flex align-items-center justify-footer  flex-wrap gap-2">
+          <p className="mb-0 text-muted" style={{ fontSize: "13px" }}>
+            <span style={{ color: "black" }}>Copyright ©</span> 2025. LİSTENSİ
+            OFİS MALZEMELERİ VE TEDARİK HİZMETLERİ TİCARET LIMITED ŞİRKETİ
+          </p>
+          <img
+            src={footerIcon}
+            className="img-fluid"
+            style={{ maxHeight: "28px" }}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
