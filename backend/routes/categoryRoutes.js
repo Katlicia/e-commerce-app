ab557d18f5df97b560ddb8e47af8690c841c3c74
Katlicia <yusuful80@gmail.com>
2026-03-26 10:25:52 +0300

    new routes with subcategories

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const { getCategories, getCategoryBySlug, createCategory, deleteCategory } = require('../controllers/categoryController');
-const { authenticationMiddle, isAdmin } = require('../middleware/auth');
+const express = require("express");
+const {
+  getCategories,
+  getRootCategories,
+  getChildren,
+  getCategoryBySlug,
+  createCategory,
+  deleteCategory,
+} = require("../controllers/categoryController");
+const { authenticationMiddle, isAdmin } = require("../middleware/auth");
 const router = express.Router();
 
-router.get('/categories', getCategories);
-router.get('/categories/:slug', getCategoryBySlug);
-router.post('/categories/new', authenticationMiddle, isAdmin, createCategory);
-router.delete('/categories/:id', authenticationMiddle, isAdmin, deleteCategory);
+router.get("/categories", getCategories);
+router.get("/categories/roots", getRootCategories);
+router.get("/categories/:slug/children", getChildren);
+router.get("/categories/:slug", getCategoryBySlug);
+router.post("/categories/new", authenticationMiddle, isAdmin, createCategory);
+router.delete("/categories/:id", authenticationMiddle, isAdmin, deleteCategory);
 
 module.exports = router;
