e102fd3492d244a3df62629c28882ab382e2b203
Katlicia <yusuful80@gmail.com>
2026-03-26 17:06:02 +0300

    add general reducer to store configuration

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,12 +1,14 @@
-import { configureStore, createReducer } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productSlice";
 import authReducer from "./authSlice";
 import cartReducer from "./cartSlice";
+import generalReducer from "./generalSlice";
 
 export const store = configureStore({
   reducer: {
     product: productReducer,
     auth: authReducer,
     cart: cartReducer,
+    general: generalReducer,
   },
 });
