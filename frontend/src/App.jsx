c96803cbb9679b731750f56c59bb234804f42632
Katlicia <yusuful80@gmail.com>
2026-03-26 15:25:16 +0300

    add router for product details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,10 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
 import Header from "./layout/Header.jsx";
 import Footer from "./layout/Footer.jsx";
-import Carousel from "./components/Carousel.jsx";
-import Featured from "./components/Featured.jsx";
-import ProductList from "./components/ProductList.jsx";
-import HeaderLinks from "./components/HeaderLinks.jsx";
-import AdBanners from "./components/AdBanners.jsx";
-import CategoryRow from "./components/CategoryRow.jsx";
-import Brands from "./components/Brands.jsx";
-import PopularLinks from "./components/PopularLinks.jsx";
 import TopBar from "./components/Topbar.jsx";
-import AdBar from "./components/AdBar.jsx";
-import DealOfDay from "./components/DealOfDay.jsx";
 import CartDrawer from "./components/CartDrawer.jsx";
 
 import Home from "./components/Home.jsx";
@@ -38,6 +29,7 @@ function App() {
           element={user ? <Navigate to="/" /> : <RegisterPage />}
         />
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<ProductDetails />} />
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
