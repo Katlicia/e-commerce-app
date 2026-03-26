8fecc1c792a78e3d8c674c26d0c8e16e31ff155b
Katlicia <yusuful80@gmail.com>
2026-03-26 15:23:11 +0300

    product slice reducers added

--- frontend/src/redux/productSlice.jsx
+++ frontend/src/redux/productSlice.jsx
@@ -1,15 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const BASE_URL = "http://localhost:5000";
 
 const initialState = {
   products: [],
+  product: {},
   loading: false,
 };
+
+export const getProducts = createAsyncThunk("products", async () => {
+  const response = await fetch(`${BASE_URL}/products`);
+  return await response.json();
+});
+
+export const getProductDetail = createAsyncThunk("product", async (id) => {
+  const response = await fetch(`${BASE_URL}/products/${id}`);
+  return await response.json();
+});
 
 export const productSlice = createSlice({
   name: "product",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(getProducts.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getProducts.fulfilled, (state, action) => {
+      state.loading = false;
+      state.products = action.payload;
+    });
+    builder.addCase(getProductDetail.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getProductDetail.fulfilled, (state, action) => {
+      state.loading = false;
+      state.product = action.payload;
+    });
+  },
 });
 
 export const {} = productSlice.actions;
