75678397da20af2c435edd614a48957e2aa7b90b
Katlicia <yusuful80@gmail.com>
2026-03-26 15:24:12 +0300

    fetch data from backend

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,98 +1,99 @@
 import { useRef, useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
-import tempIcon from "../assets/temp.png";
 import "../styles/ProductList.css";
 import "../styles/Chances.css";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 import BannerCard from "./BannerCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../redux/productSlice";
 
-const products = [
-  {
-    id: 1,
-    name: "Selpak Extra Rulo Kağıt Havlu 8'Li Paket",
-    price: "128.00",
-    image: tempIcon,
-    badge: null,
-    code: 123123,
-    rating: 4,
-    reviewCount: 68,
-  },
-  {
-    id: 11,
-    name: "Selpak Extra Rulo Kağıt Havlu 8'Li Paket Selpak Extra Rulo Kağıt Havlu 8'Li Paket",
-    price: "128.00",
-    image: tempIcon,
-    badge: null,
-    code: 123123,
-    rating: 4,
-    reviewCount: 68,
-  },
-  {
-    id: 2,
-    name: "Doğuş Filiz Çay 1kg",
-    price: "96.00",
-    image: tempIcon,
-    badge: "yeni",
-    code: 123124,
-    rating: 5,
-    reviewCount: 120,
-  },
-  {
-    id: 3,
-    name: "Ülker Cizi Peynirli Kraker 75g",
-    price: "28.00",
-    image: tempIcon,
-    badge: "gunun-firsati",
-    discount: "Seçenekli Ürün",
-    code: 123125,
-    rating: 4,
-    reviewCount: 44,
-  },
-  {
-    id: 4,
-    name: "Nescafe Classic 1kg",
-    price: "128.00",
-    image: tempIcon,
-    badge: "en-cok-satan",
-    code: 123126,
-    rating: 5,
-    reviewCount: 200,
-  },
-  {
-    id: 5,
-    name: "Fiskars Makas Seti",
-    price: "134.00",
-    oldPrice: "148.00",
-    image: tempIcon,
-    badge: "indirimli",
-    discount: "%10 indirim",
-    code: 123127,
-    rating: 3,
-    reviewCount: 31,
-  },
-  {
-    id: 6,
-    name: "Selpak Extra Rulo Kağıt Havlu 8'Li Paket",
-    price: "128.00",
-    image: tempIcon,
-    badge: null,
-    code: 123128,
-    rating: 4,
-    reviewCount: 55,
-  },
-  {
-    id: 7,
-    name: "Jacobs Monarch Filtre Kahve 500g",
-    price: "210.00",
-    oldPrice: "240.00",
-    image: tempIcon,
-    badge: "indirimli",
-    discount: "%12 indirim",
-    code: 123129,
-    rating: 5,
-    reviewCount: 88,
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: "Selpak Extra Rulo Kağıt Havlu 8'Li Paket",
+//     price: "128.00",
+//     image: tempIcon,
+//     badge: null,
+//     code: 123123,
+//     rating: 4,
+//     reviewCount: 68,
+//   },
+//   {
+//     id: 11,
+//     name: "Selpak Extra Rulo Kağıt Havlu 8'Li Paket Selpak Extra Rulo Kağıt Havlu 8'Li Paket",
+//     price: "128.00",
+//     image: tempIcon,
+//     badge: null,
+//     code: 123123,
+//     rating: 4,
+//     reviewCount: 68,
+//   },
+//   {
+//     id: 2,
+//     name: "Doğuş Filiz Çay 1kg",
+//     price: "96.00",
+//     image: tempIcon,
+//     badge: "yeni",
+//     code: 123124,
+//     rating: 5,
+//     reviewCount: 120,
+//   },
+//   {
+//     id: 3,
+//     name: "Ülker Cizi Peynirli Kraker 75g",
+//     price: "28.00",
+//     image: tempIcon,
+//     badge: "gunun-firsati",
+//     discount: "Seçenekli Ürün",
+//     code: 123125,
+//     rating: 4,
+//     reviewCount: 44,
+//   },
+//   {
+//     id: 4,
+//     name: "Nescafe Classic 1kg",
+//     price: "128.00",
+//     image: tempIcon,
+//     badge: "en-cok-satan",
+//     code: 123126,
+//     rating: 5,
+//     reviewCount: 200,
+//   },
+//   {
+//     id: 5,
+//     name: "Fiskars Makas Seti",
+//     price: "134.00",
+//     oldPrice: "148.00",
+//     image: tempIcon,
+//     badge: "indirimli",
+//     discount: "%10 indirim",
+//     code: 123127,
+//     rating: 3,
+//     reviewCount: 31,
+//   },
+//   {
+//     id: 6,
+//     name: "Selpak Extra Rulo Kağıt Havlu 8'Li Paket",
+//     price: "128.00",
+//     image: tempIcon,
+//     badge: null,
+//     code: 123128,
+//     rating: 4,
+//     reviewCount: 55,
+//   },
+//   {
+//     id: 7,
+//     name: "Jacobs Monarch Filtre Kahve 500g",
+//     price: "210.00",
+//     oldPrice: "240.00",
+//     image: tempIcon,
+//     badge: "indirimli",
+//     discount: "%12 indirim",
+//     code: 123129,
+//     rating: 5,
+//     reviewCount: 88,
+//   },
+// ];
 
 const TARGET = new Date(
   Date.now() + 11 * 3600 * 1000 + 43 * 60 * 1000 + 35 * 1000,
@@ -112,11 +113,18 @@ function ProductList({ title, settings = {} }) {
   const rowRef = useRef(null);
   const [time, setTime] = useState(getTimeLeft());
 
+  const dispatch = useDispatch();
+  const { products, loading } = useSelector((state) => state.product);
+
   useEffect(() => {
     if (!showTimer) return;
     const id = setInterval(() => setTime(getTimeLeft()), 1000);
     return () => clearInterval(id);
   }, [showTimer]);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
 
   function scrollLeft() {
     rowRef.current.scrollBy({ left: -600, behavior: "smooth" });
@@ -164,7 +172,7 @@ function ProductList({ title, settings = {} }) {
           )}
 
           {products.map((product) => (
-            <div key={product.id} className="col-6 col-md-4 col-lg-5-custom">
+            <div key={product._id} className="col-6 col-md-4 col-lg-5-custom">
               <ProductCard product={product} />
             </div>
           ))}
