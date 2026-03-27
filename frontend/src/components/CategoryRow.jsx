b5bdc3821fe90a213489d98f0303b63ed32c9ee0
Katlicia <yusuful80@gmail.com>
2026-03-27 15:35:43 +0300

    fetches according to categorySlug

--- frontend/src/components/CategoryRow.jsx
+++ frontend/src/components/CategoryRow.jsx
@@ -1,69 +1,41 @@
 import ProductCard from "./ProductCard";
 import BannerCard from "./BannerCard";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../styles/ProductList.css";
 import "../styles/CategoryRow.css";
 import okulBanner from "../assets/Banners/okul.png";
 import arabaBanner from "../assets/Banners/araba.png";
-import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../redux/productSlice";
+import Loading from "./Loading";
 
-// const products = [
-//   {
-//     id: 1,
-//     name: "Faber Castell Sulu Boya 21 Renk Büyük Boy",
-//     price: "128.00",
-//     oldPrice: "148.00",
-//     image: tempIcon,
-//     badge: null,
-//     code: 123123,
-//     rating: 4,
-//     reviewCount: 68,
-//   },
-//   {
-//     id: 2,
-//     name: "Fatih Pastel Boya 12 Renk King Size",
-//     price: "128.00",
-//     oldPrice: "148.00",
-//     image: tempIcon,
-//     badge: null,
-//     code: 123124,
-//     rating: 5,
-//     reviewCount: 88,
-//   },
-//   {
-//     id: 3,
-//     name: "Selpak Extra Rulo Kağıt Havlu 8'Li Paket",
-//     price: "128.00",
-//     oldPrice: "148.00",
-//     image: tempIcon,
-//     badge: null,
-//     code: 123125,
-//     rating: 4,
-//     reviewCount: 68,
-//   },
-//   {
-//     id: 4,
-//     name: "Selpak Extra Rulo Kağıt Havlu 8'Li Paket",
-//     price: "128.00",
-//     oldPrice: "148.00",
-//     image: tempIcon,
-//     badge: null,
-//     code: 123126,
-//     rating: 5,
-//     reviewCount: 88,
-//   },
-// ];
+const BASE_URL = "http://localhost:5000";
 
-function CategorySection({ title, image }) {
+function CategorySection({ title, image, categorySlug }) {
   const rowRef = useRef(null);
-
-  const dispatch = useDispatch();
-  const { products, loading } = useSelector((state) => state.product);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
+    if (!categorySlug) return;
+
+    const fetchProducts = async () => {
+      setLoading(true);
+      try {
+        const catRes = await fetch(`${BASE_URL}/categories/${categorySlug}`);
+        const category = await catRes.json();
+        const prodRes = await fetch(
+          `${BASE_URL}/products?category=${category._id}`,
+        );
+        const data = await prodRes.json();
+        setProducts(Array.isArray(data) ? data : []);
+      } catch (err) {
+        console.error("CategorySection fetch error:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, [categorySlug]);
 
   return (
     <div>
@@ -74,16 +46,20 @@ function CategorySection({ title, image }) {
         </a>
       </div>
       <div className="position-relative">
-        <div className="row g-2 product-row" ref={rowRef}>
-          <div className="col-category">
-            <BannerCard image={image} />
-          </div>
-          {products.map((product) => (
-            <div key={product._id} className="col-category">
-              <ProductCard product={product} />
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="row g-2 product-row" ref={rowRef}>
+            <div className="col-category">
+              <BannerCard image={image} />
             </div>
-          ))}
-        </div>
+            {products.map((product) => (
+              <div key={product._id} className="col-category">
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
@@ -94,10 +70,18 @@ function CategoryRow({ left, right }) {
     <div className="container my-4">
       <div className="row g-4">
         <div className="col-12 col-lg-6">
-          <CategorySection title={left} image={okulBanner} />
+          <CategorySection
+            title={left.title}
+            image={okulBanner}
+            categorySlug={left.slug}
+          />
         </div>
         <div className="col-12 col-lg-6">
-          <CategorySection title={right} image={arabaBanner} />
+          <CategorySection
+            title={right.title}
+            image={arabaBanner}
+            categorySlug={right.slug}
+          />
         </div>
       </div>
     </div>
