f169f2eaa005693f76afe4736260e66a675f353d
Katlicia <yusuful80@gmail.com>
2026-03-30 15:12:32 +0300

    update address and password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,9 @@ const {
   deleteUser,
   addUserAddresses,
   getUserAddresses,
+  updateUserAddress,
   getUserDetail,
+  changePassword,
 } = require("../controllers/userController");
 const { authenticationMiddle } = require("../middleware/auth");
 
@@ -16,6 +18,8 @@ router.get("/users", authenticationMiddle, getUsers);
 router.get("/users/me", authenticationMiddle, getUserDetail);
 router.get("/users/me/addresses", authenticationMiddle, getUserAddresses);
 router.post("/users/me/addresses", authenticationMiddle, addUserAddresses);
+router.put("/users/me/addresses", authenticationMiddle, updateUserAddress);
+router.put("/users/me/password", authenticationMiddle, changePassword);
 router.get("/users/:id", getUserById);
 router.put("/users/:id", authenticationMiddle, updateUser);
 router.delete("/users/:id", authenticationMiddle, deleteUser);
