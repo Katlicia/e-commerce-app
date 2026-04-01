c77ea8dafa982ae071f10abe307b26500129b50f
Katlicia <yusuful80@gmail.com>
2026-04-01 17:19:28 +0300

    upddate category added

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -57,6 +57,62 @@ exports.createCategory = async (req, res) => {
   res.json(category);
 };
 
+exports.updateCategory = async (req, res) => {
+  const { name, slug, parent, subcategories } = req.body;
+
+  let parentId = undefined;
+  if (parent !== undefined) {
+    if (!parent) {
+      parentId = null;
+    } else {
+      const parentDoc = await Category.findOne({ slug: parent });
+      if (!parentDoc)
+        return res.status(404).json({ message: "Parent kategori bulunamadı." });
+      parentId = parentDoc._id;
+    }
+  }
+
+  const updateData = {};
+  if (name !== undefined) updateData.name = name;
+  if (slug !== undefined) updateData.slug = slug;
+  if (parentId !== undefined) updateData.parent = parentId;
+
+  const category = await Category.findByIdAndUpdate(req.params.id, updateData, {
+    new: true,
+    runValidators: true,
+  });
+  if (!category)
+    return res.status(404).json({ message: "Kategori bulunamadı." });
+
+  // Subcategories
+  if (Array.isArray(subcategories)) {
+    const existingSubs = await Category.find({ parent: category._id });
+    const incomingIds = subcategories.filter((s) => s._id).map((s) => s._id);
+
+    //////
+    for (const sub of existingSubs) {
+      if (!incomingIds.includes(sub._id.toString())) {
+        await Category.findByIdAndDelete(sub._id);
+      }
+    }
+
+    // Update or create subcategories
+    for (const sub of subcategories) {
+      if (sub._id) {
+        await Category.findByIdAndUpdate(
+          sub._id,
+          { name: sub.name, slug: sub.slug },
+          { runValidators: true },
+        );
+      } else {
+        await Category.create({ name: sub.name, slug: sub.slug, parent: category._id });
+      }
+    }
+  }
+
+  res.json({ message: "Kategori güncellendi" });
+};
+
 exports.deleteCategory = async (req, res) => {
   const category = await Category.findByIdAndDelete(req.params.id);
   if (!category)
