44afc86bb7443d1117c582565403afe6e8243fc4
Katlicia <yusuful80@gmail.com>
2026-04-02 15:20:58 +0300

    return order added to route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, getUserOrders, cancelOrder } = require("../controllers/orderController");
+const { createOrder, getUserOrders, cancelOrder, returnOrder } = require("../controllers/orderController");
 const { authenticationMiddle, optionalAuth } = require("../middleware/auth");
 
 router.post("/orders", optionalAuth, createOrder);
 router.get("/orders/me", authenticationMiddle, getUserOrders);
 router.patch("/orders/:id/cancel", authenticationMiddle, cancelOrder);
+router.patch("/orders/:id/return", authenticationMiddle, returnOrder);
 
 module.exports = router;
