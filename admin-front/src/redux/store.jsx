62c6118bf83dc33992f419cb7cb18ee2f02f9319
Katlicia <yusuful80@gmail.com>
2026-04-03 16:35:48 +0300

    add notification reducer to store

--- admin-front/src/redux/store.jsx
+++ admin-front/src/redux/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import adminReducer from "./adminSlice";
 import authReducer from "./authSlice";
 import productReducer from "./productSlice";
+import notificationReducer from "./notificationSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     admin: adminReducer,
     product: productReducer,
+    notification: notificationReducer,
   },
 });
