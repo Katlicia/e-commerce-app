6cde46c6fecd651f824a05eae191280cc0f8fa34
Katlicia <yusuful80@gmail.com>
2026-04-03 16:35:06 +0300

    new style and options for the homepage

--- admin-front/src/Components/AdminHome.jsx
+++ admin-front/src/Components/AdminHome.jsx
@@ -6,19 +6,40 @@ function AdminPage() {
 
   return (
     <div className="container my-5">
-      <h1 className="text-center mb-5">Admin Paneli</h1>
-      <div className="d-flex gap-4 orange-text">
+      <div className="d-flex gap-4 orange-text flex-column">
         <div
           className="admin-panel-card flex-fill d-flex align-items-center justify-content-center"
-          onClick={() => navigate("/admin/products")}
+          onClick={() => navigate("/admin/orders")}
         >
-          <h2>Ürünler</h2>
+          <h2>Siparişler</h2>
         </div>
-        <div
-          className="admin-panel-card flex-fill d-flex align-items-center justify-content-center"
-          onClick={() => navigate("/admin/categories")}
-        >
-          <h2>Kategoriler</h2>
+        <div className="d-flex gap-4">
+          <div
+            className="admin-panel-card flex-fill d-flex align-items-center justify-content-center"
+            onClick={() => navigate("/admin/products")}
+          >
+            <h2>Ürünler</h2>
+          </div>
+          <div
+            className="admin-panel-card flex-fill d-flex align-items-center justify-content-center"
+            onClick={() => navigate("/admin/categories")}
+          >
+            <h2>Kategoriler</h2>
+          </div>
+        </div>
+        <div className="d-flex gap-4">
+          <div
+            className="admin-panel-card flex-fill d-flex align-items-center justify-content-center"
+            onClick={() => navigate("/admin/cargo")}
+          >
+            <h2>Kargo</h2>
+          </div>
+          <div
+            className="admin-panel-card flex-fill d-flex align-items-center justify-content-center"
+            onClick={() => navigate("/admin/tax-settings")}
+          >
+            <h2>Vergi Ayarları</h2>
+          </div>
         </div>
       </div>
     </div>
