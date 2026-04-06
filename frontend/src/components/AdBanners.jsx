9127c59bcedf763939486cb061a046fc178626b6
Katlicia <yusuful80@gmail.com>
2026-04-06 13:39:39 +0300

    ad banners now fetch from server

--- frontend/src/components/AdBanners.jsx
+++ frontend/src/components/AdBanners.jsx
@@ -3,25 +3,32 @@ import "../styles/AdBanners.css";
 import { useDispatch } from "react-redux";
 import { getKeyword } from "../redux/generalSlice";
 
-function AdBanners({ banners = [], slugs = [] }) {
+function AdBanners({ images = [] }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const sendKeyword = (value) => {
-    dispatch(getKeyword(value));
-    navigate("/products");
+  const handleClick = (link) => {
+    if (!link) return;
+    if (link.startsWith("cat:")) {
+      dispatch(getKeyword(""));
+      navigate("/products?category=" + link.slice(4));
+    } else if (link.startsWith("brand:")) {
+      dispatch(getKeyword(""));
+      navigate("/products?brand=" + encodeURIComponent(link.slice(6)));
+    } else {
+      dispatch(getKeyword(""));
+      navigate("/products?category=" + link);
+    }
   };
 
   return (
     <div className="container my-4 d-flex gap-1 banner-list">
-      {banners.map((src, i) => (
+      {images.map((img, i) => (
         <div key={i} className="flex-fill">
           <img
-            className={`rounded img-fluid w-100${slugs[i] ? " banner-clickable" : ""}`}
-            src={src}
-            onClick={() => {
-              sendKeyword(slugs[i]);
-            }}
+            className={`rounded img-fluid w-100${img.link ? " banner-clickable" : ""}`}
+            src={img.url}
+            onClick={() => handleClick(img.link)}
           />
         </div>
       ))}
