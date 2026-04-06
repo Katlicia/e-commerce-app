f572344fa640bdc1467706d6496651215a0ae291
Katlicia <yusuful80@gmail.com>
2026-04-06 18:01:41 +0300

    products panel draft

---
+++ admin-front/src/Components/ProductsPanel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ProductsPanel() {
+  return (
+    <div className="p-4">
+      <div
+        style={{
+          background: "#fff",
+          borderRadius: "12px",
+          padding: "24px",
+          boxShadow: "0 1px 4px rgba(0,0,0,0.2)",
+        }}
+      >
+        <div className="fw-bold mb-4 h3">Ürünler</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductsPanel;
