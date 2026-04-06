4ac85de96bee2454503df99e7f7be81539a0e922
Katlicia <yusuful80@gmail.com>
2026-04-06 13:48:18 +0300

    implement banner and home sections fetching in Home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -13,10 +13,33 @@ import banner3 from "../assets/Banners/banner3.png";
 import liptonBanner from "../assets/Banners/lipton-banner.png";
 import cifBanner from "../assets/Banners/cif-banner.png";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getBanner } from "../redux/bannerSlice";
+import { getHomeSections } from "../redux/homeSectionSlice";
+
+const FALLBACK_AD1 = [
+  { url: banner1, link: "" },
+  { url: banner2, link: "" },
+  { url: banner3, link: "" },
+];
+const FALLBACK_AD2 = [
+  { url: liptonBanner, link: "lipton" },
+  { url: cifBanner, link: "cif" },
+];
 
 function Home() {
+  const dispatch = useDispatch();
+  const adBanner1 = useSelector((state) => state.banner.banners["adbanner-1"]);
+  const adBanner2 = useSelector((state) => state.banner.banners["adbanner-2"]);
+  const sections = useSelector((state) => state.homeSection.sections);
+
+  const getSection = (key) => sections.find((s) => s.key === key) || {};
+
   useEffect(() => {
     window.scrollTo(0, 0);
+    dispatch(getBanner("adbanner-1"));
+    dispatch(getBanner("adbanner-2"));
+    dispatch(getHomeSections());
   }, []);
 
   return (
@@ -25,25 +48,52 @@ function Home() {
       <Carousel />
       <Featured />
       <ProductList
-        title="Kaçırılmayacak Fırsatlar"
-        settings={{ showTimer: true, banner: banner, badge: "indirimli" }}
+        title={getSection("deals").title || "Kaçırılmayacak Fırsatlar"}
+        settings={{
+          showTimer: getSection("deals").showTimer ?? false,
+          timerEnd: getSection("deals").timerEnd,
+          banner: getSection("deals").banner?.url || null,
+          badge: getSection("deals").badge || "indirimli",
+        }}
       />
-      <ProductList title="Yeni Gelenler" settings={{ badge: "yeni" }} />
-      <AdBanners banners={[banner1, banner2, banner3]} />
       <ProductList
-        title="Önerilen Ürünler"
-        settings={{ badge: "en-cok-satan" }}
+        title={getSection("new").title || "Yeni Gelenler"}
+        settings={{
+          showTimer: getSection("new").showTimer ?? false,
+          timerEnd: getSection("new").timerEnd,
+          banner: getSection("new").banner?.url || null,
+          badge: getSection("new").badge || "yeni",
+        }}
       />
-      <CategoryRow
-        left={{ title: "Okul Kırtasiye", slug: "okul-ve-kirtasiye" }}
-        right={{ title: "Temizlik", slug: "temizlik" }}
+      <AdBanners images={adBanner1?.length > 0 ? adBanner1 : FALLBACK_AD1} />
+      <ProductList
+        title={getSection("featured").title || "Önerilen Ürünler"}
+        settings={{
+          showTimer: getSection("featured").showTimer ?? false,
+          timerEnd: getSection("featured").timerEnd,
+          banner: getSection("featured").banner?.url || null,
+          badge: getSection("featured").badge || "en-cok-satan",
+        }}
       />
+      {getSection("categoryRow-1").left && getSection("categoryRow-1").right ? (
+        <CategoryRow
+          left={getSection("categoryRow-1").left}
+          right={getSection("categoryRow-1").right}
+        />
+      ) : null}
       <AdBar />
-      <ProductList title="Son Gezdiğin Ürünler" />
-      <AdBanners
-        banners={[liptonBanner, cifBanner]}
-        slugs={["lipton", "cif"]}
+      <ProductList
+        title={getSection("recent").title || "Son Gezdiğin Ürünler"}
+        settings={{
+          showTimer: getSection("recent").showTimer ?? false,
+          timerEnd: getSection("recent").timerEnd,
+          banner: getSection("recent").banner?.url || null,
+          badge: getSection("recent").badge || "",
+          recentlyViewed: true,
+        }}
       />
+
+      <AdBanners images={adBanner2?.length > 0 ? adBanner2 : FALLBACK_AD2} />
       <DealOfDay />
     </>
   );
