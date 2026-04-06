a08e526017f6b1628583fceb8b206a489ab2fcf0
Katlicia <yusuful80@gmail.com>
2026-04-06 13:43:12 +0300

    topbar fetches from server

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,10 +1,22 @@
 import "../styles/TopBar.css";
 import topbarImg from "../assets/Banners/topbar.png";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getBanner } from "../redux/bannerSlice";
 
 function TopBar() {
+  const dispatch = useDispatch();
+  const images = useSelector((state) => state.banner.banners["topbar"]);
+
+  useEffect(() => {
+    dispatch(getBanner("topbar"));
+  }, []);
+
+  const src = images?.length > 0 ? images[0].url : topbarImg;
+
   return (
     <div className="topbar">
-      <img src={topbarImg} alt="topbar" />
+      <img src={src} alt="topbar" />
     </div>
   );
 }
