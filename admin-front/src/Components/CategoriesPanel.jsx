9a214d9de178fd0397c2c19a59c3a149a27a1188
Katlicia <yusuful80@gmail.com>
2026-04-06 18:01:57 +0300

    categories panel draft

---
+++ admin-front/src/Components/CategoriesPanel.jsx
@@ -0,0 +1,18 @@
+function CategoriesPanel() {
+  return (
+    <div className="p-4">
+      <div
+        style={{
+          background: "#fff",
+          borderRadius: "12px",
+          padding: "24px",
+          boxShadow: "0 1px 4px rgba(0,0,0,0.2)",
+        }}
+      >
+        <div className="fw-bold mb-4 h3">Kategoriler</div>
+      </div>
+    </div>
+  );
+}
+
+export default CategoriesPanel;
