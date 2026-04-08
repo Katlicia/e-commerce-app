c742ff9d76293b96161cbdf8dc954559128a63a3
Katlicia <yusuful80@gmail.com>
2026-04-08 16:15:50 +0300

    remove unnecesary imports and fallbacks for adbanners

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,39 +6,18 @@ import AdBanners from "./AdBanners";
 import CategoryRow from "./CategoryRow";
 import AdBar from "./AdBar";
 import DealOfDay from "./DealOfDay";
-import banner from "../assets/Banners/banner.png";
-import banner1 from "../assets/Banners/banner1.png";
-import banner2 from "../assets/Banners/banner2.png";
-import banner3 from "../assets/Banners/banner3.png";
-import liptonBanner from "../assets/Banners/lipton-banner.png";
-import cifBanner from "../assets/Banners/cif-banner.png";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getBanner } from "../redux/bannerSlice";
 import { getHomeSections } from "../redux/homeSectionSlice";
-
-const FALLBACK_AD1 = [
-  { url: banner1, link: "" },
-  { url: banner2, link: "" },
-  { url: banner3, link: "" },
-];
-const FALLBACK_AD2 = [
-  { url: liptonBanner, link: "lipton" },
-  { url: cifBanner, link: "cif" },
-];
 
 function Home() {
   const dispatch = useDispatch();
-  const adBanner1 = useSelector((state) => state.banner.banners["adbanner-1"]);
-  const adBanner2 = useSelector((state) => state.banner.banners["adbanner-2"]);
   const sections = useSelector((state) => state.homeSection.sections);
 
   const getSection = (key) => sections.find((s) => s.key === key) || {};
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    dispatch(getBanner("adbanner-1"));
-    dispatch(getBanner("adbanner-2"));
     dispatch(getHomeSections());
   }, []);
 
@@ -65,7 +44,7 @@ function Home() {
           badge: getSection("new").badge || "yeni",
         }}
       />
-      <AdBanners images={adBanner1?.length > 0 ? adBanner1 : FALLBACK_AD1} />
+      <AdBanners type="adbanner-1" />
       <ProductList
         title={getSection("featured").title || "En Çok Satanlar"}
         settings={{
@@ -94,7 +73,7 @@ function Home() {
         }}
       />
 
-      <AdBanners images={adBanner2?.length > 0 ? adBanner2 : FALLBACK_AD2} />
+      <AdBanners type="adbanner-2" />
       <DealOfDay />
     </>
   );
