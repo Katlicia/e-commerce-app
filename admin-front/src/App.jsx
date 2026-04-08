b26b52700f91f54f44695d566a6bd504c07eb250
Katlicia <yusuful80@gmail.com>
2026-04-08 16:47:09 +0300

    refactor routing in App component by removing unused admin routes

--- admin-front/src/App.jsx
+++ admin-front/src/App.jsx
@@ -1,17 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LoginPage from "./Components/LoginPage.jsx";
-import AdminHome from "./Components/AdminHome.jsx";
-import AdminHeader from "./Components/AdminHeader.jsx";
 import AdminProductFormPage from "./Components/AdminProductFormPage.jsx";
-import AdminProductsPage from "./Components/AdminProductsPage.jsx";
-import AdminCategoriesPage from "./Components/AdminCategoriesPage.jsx";
-import AdminCargoPage from "./Components/AdminCargoPage.jsx";
-import AdminTaxSettingsPage from "./Components/AdminTaxSettingsPage.jsx";
-import AdminOrdersPage from "./Components/AdminOrdersPage.jsx";
-import AdminOrderDetailPage from "./Components/AdminOrderDetailPage.jsx";
-import AdminBannersPage from "./Components/AdminBannersPage.jsx";
-import AdminHomeSectionsPage from "./Components/AdminHomeSectionsPage.jsx";
 import AdminToast from "./Components/AdminToast.jsx";
 import Home from "./Components/Home.jsx";
 
@@ -20,69 +10,17 @@ function App() {
 
   return (
     <BrowserRouter>
-      {/* <AdminHeader /> */}
       <AdminToast />
       <Routes>
-        <Route
-          path="/"
-          // element={user ? <AdminHome /> : <Navigate to="/login" />}
-          element={user ? <Home /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/login"
-          element={user ? <Navigate to="/" /> : <LoginPage />}
-        />
-        <Route
-          path="/admin/products"
-          element={user?.isAdmin ? <AdminProductsPage /> : <Navigate to="/" />}
-        />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
         <Route
           path="/admin/products/new"
-          element={
-            user?.isAdmin ? <AdminProductFormPage /> : <Navigate to="/" />
-          }
+          element={user?.isAdmin ? <AdminProductFormPage /> : <Navigate to="/" />}
         />
         <Route
           path="/admin/products/:id/edit"
-          element={
-            user?.isAdmin ? <AdminProductFormPage /> : <Navigate to="/" />
-          }
-        />
-        <Route
-          path="/admin/categories"
-          element={
-            user?.isAdmin ? <AdminCategoriesPage /> : <Navigate to="/" />
-          }
-        />
-        <Route
-          path="/admin/cargo"
-          element={user?.isAdmin ? <AdminCargoPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/admin/tax-settings"
-          element={
-            user?.isAdmin ? <AdminTaxSettingsPage /> : <Navigate to="/" />
-          }
-        />
-        <Route
-          path="/admin/orders"
-          element={user?.isAdmin ? <AdminOrdersPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/admin/orders/:id"
-          element={
-            user?.isAdmin ? <AdminOrderDetailPage /> : <Navigate to="/" />
-          }
-        />
-        <Route
-          path="/admin/banners"
-          element={user?.isAdmin ? <AdminBannersPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/admin/home-sections"
-          element={
-            user?.isAdmin ? <AdminHomeSectionsPage /> : <Navigate to="/" />
-          }
+          element={user?.isAdmin ? <AdminProductFormPage /> : <Navigate to="/" />}
         />
         <Route path="*" element={user ? <Home /> : <Navigate to="/login" />} />
       </Routes>
