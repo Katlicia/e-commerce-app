43bd927afbe957f788684fd849b2d907804df1ad
Katlicia <yusuful80@gmail.com>
2026-04-13 17:40:40 +0300

    add refund and cancel to router

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createPayment } = require("../controllers/paymentController");
-const { optionalAuth } = require("../middleware/auth");
+const { createPayment, cancelPayment, refundPayment } = require("../controllers/paymentController");
+const { optionalAuth, authenticationMiddle } = require("../middleware/auth");
 
 router.post("/api/payment", optionalAuth, createPayment);
+router.patch("/api/payment/orders/:id/cancel", authenticationMiddle, cancelPayment);
+router.patch("/api/payment/orders/:id/refund", authenticationMiddle, refundPayment);
 
 module.exports = router;
