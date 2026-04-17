3fd3894313c05e439230c31cc1e3d6d654a3b497
Katlicia <yusuful80@gmail.com>
2026-04-17 15:36:24 +0300

    delete and update sections

--- backend/routes/homeSectionRoutes.js
+++ backend/routes/homeSectionRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getHomeSections, updateHomeSection } = require("../controllers/homeSectionController");
+const { getHomeSections, createHomeSection, updateHomeSection, deleteHomeSection } = require("../controllers/homeSectionController");
 const { authenticationMiddle, isAdmin } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/home-sections", getHomeSections);
+router.post("/home-sections", authenticationMiddle, isAdmin, createHomeSection);
 router.put("/home-sections/:key", authenticationMiddle, isAdmin, updateHomeSection);
+router.delete("/home-sections/:key", authenticationMiddle, isAdmin, deleteHomeSection);
 
 module.exports = router;
