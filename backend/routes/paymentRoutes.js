84f6a179d9fb4cbb6d74cd0412032c65fd54b894
Katlicia <yusuful80@gmail.com>
2026-04-17 15:34:29 +0300

    add new payment controllers to router

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createPayment, cancelPayment, refundPayment } = require("../controllers/paymentController");
-const { optionalAuth, authenticationMiddle } = require("../middleware/auth");
+const { createPayment, cancelPayment, refundPayment, adminCancelPayment, adminRefundPayment } = require("../controllers/paymentController");
+const { optionalAuth, authenticationMiddle, isAdmin } = require("../middleware/auth");
 
 router.post("/api/payment", optionalAuth, createPayment);
 router.patch("/api/payment/orders/:id/cancel", authenticationMiddle, cancelPayment);
 router.patch("/api/payment/orders/:id/refund", authenticationMiddle, refundPayment);
+router.patch("/api/payment/admin/orders/:id/cancel", authenticationMiddle, isAdmin, adminCancelPayment);
+router.patch("/api/payment/admin/orders/:id/refund", authenticationMiddle, isAdmin, adminRefundPayment);
 
 module.exports = router;
