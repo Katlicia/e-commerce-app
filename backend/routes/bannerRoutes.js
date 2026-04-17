dc04676f8f5caa60e8f96d8ff3c7a621cf8b5a91
Katlicia <yusuful80@gmail.com>
2026-04-17 15:34:51 +0300

    add new banner controllers to router

--- backend/routes/bannerRoutes.js
+++ backend/routes/bannerRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getBanner, updateBanner } = require("../controllers/bannerController");
+const { getAllAdBanners, getBanner, updateBanner, deleteBanner } = require("../controllers/bannerController");
 const { authenticationMiddle, isAdmin } = require("../middleware/auth");
 
 const router = express.Router();
 
+router.get("/banners", authenticationMiddle, isAdmin, getAllAdBanners);
 router.get("/banners/:type", getBanner);
 router.put("/banners/:type", authenticationMiddle, isAdmin, updateBanner);
+router.delete("/banners/:type", authenticationMiddle, isAdmin, deleteBanner);
 
 module.exports = router;
