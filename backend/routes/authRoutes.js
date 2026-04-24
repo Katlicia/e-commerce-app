8a076effe33e45d31b8930ebcc91bb484573c69e
Katlicia <yusuful80@gmail.com>
2026-04-24 09:45:50 +0300

    add me to router

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,7 +6,9 @@ const {
   logout,
   forgetPassword,
   resetPassword,
+  getMe,
 } = require("../controllers/authController");
+const { authenticationMiddle } = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -14,6 +16,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/adminlogin", adminLogin);
 router.get("/logout", logout);
+router.get("/me", authenticationMiddle, getMe);
 router.post("/forgetPassword", forgetPassword);
 router.post("/reset/:token", resetPassword);
 
