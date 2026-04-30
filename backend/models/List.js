7da61feb89723e6a273094040f779c0b2b654d3c
Katlicia <yusuful80@gmail.com>
2026-04-30 11:03:01 +0300

    create list model

---
+++ backend/models/List.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const listSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("List", listSchema);
