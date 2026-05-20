687088032a53e624fa40107633ed9f72f9a35229
Katlicia <yusuful80@gmail.com>
2026-05-20 12:09:27 +0300

    add controllers to router

--- backend/routes/corporateOfferRoutes.js
+++ backend/routes/corporateOfferRoutes.js
@@ -4,6 +4,7 @@ const {
   getOffers,
   getMyOffers,
   updateOfferStatus,
+  replyToOffer,
 } = require("../controllers/corporateOfferController");
 const {
   optionalAuth,
@@ -16,6 +17,7 @@ const router = express.Router();
 router.post("/corporate-offers", optionalAuth, createOffer);
 router.get("/corporate-offers/me", authenticationMiddle, getMyOffers);
 router.get("/corporate-offers", authenticationMiddle, isAdmin, getOffers);
-router.patch("/corporate-offers/:id/status", isAdmin, updateOfferStatus);
+router.patch("/corporate-offers/:id/status", authenticationMiddle, isAdmin, updateOfferStatus);
+router.patch("/corporate-offers/:id/reply", authenticationMiddle, isAdmin, replyToOffer);
 
 module.exports = router;
